80bdf9e711237910f01d8b6b8f6cacc6029c29a2
Venkatesh <venkateshreddy.u@gmail.com>
2018-08-22 22:32:43 +0530

    updated

--- src/com/myscala/training/programs/ForLoop.scala
+++ src/com/myscala/training/programs/ForLoop.scala
@@ -7,7 +7,7 @@ object ForLoop {
          println(a) 
       }  
         println("for loop with until")
-        for( a <- 1 until 10 ){  
+        for( a <- 1 until 10 ){ //10==> 10-1 = 9 , last n ==> n-1 
          println(a) 
       }  
         println("for loop with if")
@@ -15,18 +15,18 @@ object ForLoop {
          println(a);  
       }  
         println("for loop with yield")
-        val result = for( a <- 1 to 10) yield a  
-        println("result:" + result)
-        for(i<-result){  
+        val result = for( a <- 1 to 10) yield a  //for comprehension
+         println("result:" + result)
+         for(i<-result){  
             println(i)  
-        }
+          }
         println("for loop with iterator")
         var list = List('A','B','C','D','E','F','G','H','I')          // Creating a list  
         for( i <- list){                         // Iterating the list  
             println(i)  
         }
         println("for loop with by")
-        for(i<-1 to 10 by 2){  
+        for(i<-1 to 10 by 5){  
             println(i)  
         }  
       }  
